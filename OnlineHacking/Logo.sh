9c777a16123bb14dc3c77893c83e91b869784143
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2022-04-07 20:09:38 +0530

    Update Logo.sh

--- OnlineHacking/Logo.sh
+++ OnlineHacking/Logo.sh
@@ -30,9 +30,9 @@ echo -e "\e[95m
      ██║  ██║██║  ██║╚██████╗██║  ██╗██║██║ ╚████║╚██████╔╝  
      ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝ ╚═════╝  \e[95;1m \e[0m\n"
   echo""    
-  echo -e $'\e[1;33m\e[0m\e[1;31m    ██████████\e[0m'"\e[1;37m██████████"'\e[1;33m\e[0m\e[0;32m██████████\e[0m' '\e[1;32m\e[0m\e[1;32m PUBG / BGMI PHISHING TOOL \e[0m''\e[1;37m\e[0m\e[1;37m [v 2.6] \e[0m'                                       
+  echo -e $'\e[1;33m\e[0m\e[1;31m    ██████████\e[0m'"\e[1;37m██████████"'\e[1;33m\e[0m\e[0;32m██████████\e[0m' '\e[1;32m\e[0m\e[1;32m PUBG / BGMI PHISHING TOOL \e[0m''\e[1;37m\e[0m\e[1;37m [v 2.7] \e[0m'                                       
   echo ""
-   echo -e $'\e[1;33m\e[0m\e[1;33m  [\e[0m\e[1;32m Follow Github : \e[36mhttps://github.com/OnlineHacKing/PUBG_BGMI-Phishing \e[0m \e[1;32m\e[0m\e[1;33m]\e[0m'
+   echo -e $'\e[1;33m\e[0m\e[1;33m  [\e[0m\e[1;32m Follow Github : \e[36mhttps://github.com/OnlineHacKing/PUBG-BGMI_Phishing \e[0m \e[1;32m\e[0m\e[1;33m]\e[0m'
    echo ""
       echo -e $'\e[1;37m\e[0m\e[1;37m    +-+-+-+-+-+-+ +-+-+-+-+-+-+-+ >>\e[0m'
       echo -e "\e[93m    |O|n|l|i|n|e| |H|a|c|k|i|n|g|"      
